0f50f380d5d8c681acd1e8f94b0315bfb314acc6
SilverRAN <silverwei.hang@gmail.com>
2024-06-03 17:17:49 +0900

    add test file

---
+++ test.py
@@ -0,0 +1,107 @@
+import argparse
+import logging
+import os
+import random
+import sys
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from datasets.dataset import Building_dataset
+from utils import test_single_case
+from networks.vit_seg_modeling import VisionTransformer as ViT_seg
+from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_path', type=str,
+                    default='./data/Prepared_Vaihingen', help='root dir for validation volume data')
+parser.add_argument('--ckpt_path', type=str,
+                    default='./ckpt/vai_epoch_99.pth', help='checkpoint path')
+parser.add_argument('--num_classes', type=int,
+                    default=2, help='output channel of network')
+
+parser.add_argument('--batch_size', type=int, default=1,
+                    help='batch_size per gpu')
+parser.add_argument('--img_size', type=int, default=256, help='input patch size of network input')
+parser.add_argument('--is_savenii', default=False, action="store_true", help='whether to save results during inference')
+
+parser.add_argument('--vit_name', type=str, default='R50-ViT-B_16', help='select one vit model')
+
+parser.add_argument('--test_save_dir', type=str, default='./predictions', help='saving prediction as nii!')
+parser.add_argument('--deterministic', type=int,  default=1, help='whether use deterministic training')
+
+parser.add_argument('--seed', type=int, default=1234, help='random seed')
+parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
+args = parser.parse_args()
+
+
+def inference(args, model, test_save_path=None):
+    db_test = Building_dataset(args=args, split="test")
+    testloader = DataLoader(db_test, batch_size=args.batch_size, shuffle=False, num_workers=0)
+    logging.info("{} test iterations per epoch".format(len(testloader)))
+    model.eval()
+    metric_list = 0.0
+    for i_batch, sampled_batch in tqdm(enumerate(testloader)):
+        image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
+        metric_i = test_single_case(image, label, model, classes=args.num_classes, test_save_path=test_save_path, case=case_name)
+        metric_list += np.array(metric_i)
+        logging.info('idx %d case %s mean_dice %f mean_hd95 %f' % (i_batch, case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
+    metric_list = metric_list / len(db_test)
+    for i in range(1, args.num_classes):
+        logging.info('Mean class %d mean_dice %f mean_hd95 %f mean prec %f mean rec %f ' % (i, metric_list[i-1][0], metric_list[i-1][1], metric_list[i-1][2], metric_list[i-1][3]))
+    performance = np.mean(metric_list, axis=0)[0]
+    mean_hd95 = np.mean(metric_list, axis=0)[1]
+    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
+    return "Testing Finished!"
+
+
+if __name__ == "__main__":
+
+    if not args.deterministic:
+        cudnn.benchmark = True
+        cudnn.deterministic = False
+    else:
+        cudnn.benchmark = False
+        cudnn.deterministic = True
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+    args.is_pretrain = True
+    
+    args.exp = 'MCTU_vaihingen' + str(args.img_size)
+    snapshot_path = "./Test_result/{}/{}".format(args.exp, 'MCTU')
+    snapshot_path = snapshot_path + '_pretrain' if args.is_pretrain else snapshot_path
+    snapshot_path += '_' + args.vit_name
+    snapshot_path = snapshot_path + '_vitpatch' + str(args.vit_patches_size) if args.vit_patches_size!=16 else snapshot_path
+    snapshot_path = snapshot_path+'_bs'+str(args.batch_size)
+    snapshot_path = snapshot_path + '_'+str(args.img_size)
+    snapshot_path = snapshot_path + '_s'+str(args.seed) if args.seed!=1234 else snapshot_path
+
+    config_vit = CONFIGS_ViT_seg[args.vit_name]
+    config_vit.n_classes = args.num_classes
+
+    if args.vit_name.find('R50') !=-1:
+        config_vit.patches.grid = (int(args.img_size/args.vit_patches_size), int(args.img_size/args.vit_patches_size))
+    net = ViT_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes).cuda()
+
+    net.load_state_dict(torch.load(args.ckpt_path))
+    snapshot_name = snapshot_path.split('/')[-1]
+
+    log_folder = './test_log/test_log_' + args.exp
+    os.makedirs(log_folder, exist_ok=True)
+    logging.basicConfig(filename=log_folder + '/'+snapshot_name+".txt", level=logging.INFO, format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    logging.info(str(args))
+    logging.info(snapshot_name)
+
+    if args.is_savenii:
+        args.test_save_dir = log_folder + '/predictions'
+        test_save_path = os.path.join(args.test_save_dir, args.exp, snapshot_name)
+        os.makedirs(test_save_path, exist_ok=True)
+    else:
+        test_save_path = None
+    inference(args, net, test_save_path)
