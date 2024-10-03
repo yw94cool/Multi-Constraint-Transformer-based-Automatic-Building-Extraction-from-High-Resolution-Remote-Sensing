154478f3a78a8ce32784f04c32d743dc0017cdf2
Weihang Ran <89587304+SilverRAN@users.noreply.github.com>
2024-10-03 14:57:12 +0900

    fix creating directory

--- download_ckpt.sh
+++ download_ckpt.sh
@@ -1,5 +1,5 @@
 wget https://storage.googleapis.com/vit_models/imagenet21k/R50+ViT-B_16.npz
 
-mkdir ./model/vit_checkpoint/imagenet21k
+mkdir -p ./model/vit_checkpoint/imagenet21k
 
 mv R50+ViT-B_16.npz ./model/vit_checkpoint/imagenet21k/
